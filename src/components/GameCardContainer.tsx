4f8c2f783f47fcdf8a42b38869d2152ce3daf68d
sobhan sadeghi <s_sadqinejad@dstods.com>
2023-12-02 22:21:49 +0330

    Display genres

--- src/components/GameCardContainer.tsx
+++ src/components/GameCardContainer.tsx
@@ -7,7 +7,7 @@ interface IProps {
 
 const GameCardContainer = ({ children }: IProps) => {
   return (
-    <Box width="300px" borderRadius={10} overflow="hidden">
+    <Box borderRadius={10} overflow="hidden">
       {children}
     </Box>
   );
